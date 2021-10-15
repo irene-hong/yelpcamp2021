511369b4127990338a9ffdbfa6c1ca7dc8b47172
irenehong <irene_hong@outlook.com>
2021-10-15 16:45:00 +0800

    update on 10/15

--- app.js
+++ app.js
@@ -1,74 +1,70 @@
-if (process.env.NODE_ENV !== "production"){ // in development env
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  // in development env
+  require("dotenv").config();
 }
 
-
-const express = require('express');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
+const express = require("express");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const helmet = require("helmet");
-const ExpressError = require('./utils/ExpressError');
-const path = require('path');
-const methodOverride = require('method-override');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const passport = require('passport');
-const localStrategy = require('passport-local');
-const User = require('./models/user');
+const ExpressError = require("./utils/ExpressError");
+const path = require("path");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user");
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(dbUrl);
- //mongodb://localhost:27017/yelp-camp
+//mongodb://localhost:27017/yelp-camp
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error: "));
-db.once("open", ()=>{
-    console.log('Database connected');
-})
-
-const secret = process.env.SECRET || 'thisisthesecretkey';
-
-const store = new MongoStore({
-    mongoUrl: dbUrl,
-    secret: secret,
-    touchAfter: 24 * 60 * 60 //24 hours
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+  console.log("Database connected");
 });
 
-store.on('error', function(e) {
-    console.log("session store error: ", e);
-})
+const secret = process.env.SECRET || "thisisthesecretkey";
+
+const store = new MongoStore({
+  mongoUrl: dbUrl,
+  secret: secret,
+  touchAfter: 24 * 60 * 60, //24 hours
+});
+
+store.on("error", function (e) {
+  console.log("session store error: ", e);
+});
 
 const sessionConfig = {
-    name:'yelpcccccamp',
-    secret: secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true, --> https
-        expires: Date.now() + 1000 * 60 * 60 * 24,
-        maxAge: 1000 * 60 * 60 * 24
-    },
-    store: store
-}
+  name: "yelpcccccamp",
+  secret: secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true, --> https
+    expires: Date.now() + 1000 * 60 * 60 * 24,
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+  store: store,
+};
 
-const campgroundRouter = require('./routes/campgrounds');
-const reviewRouter = require('./routes/reviews');
-const userRouter = require('./routes/users');
-
-
-
+const campgroundRouter = require("./routes/campgrounds");
+const reviewRouter = require("./routes/reviews");
+const userRouter = require("./routes/users");
 
 const app = express();
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(helmet({contentSecurityPolicy: false}));
+app.use(methodOverride("_method"));
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
@@ -78,36 +74,32 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // locals让接下来每一个route都能获得success变量的数据
-app.use((req, res, next) =>{
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
-
-
-
-app.use("/campgrounds", campgroundRouter);
-app.use('/campgrounds/:id/reviews', reviewRouter);
-app.use('/', userRouter);
-
-app.get('/', (req, res) => {
-    res.render('home')
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
+app.use("/campgrounds", campgroundRouter);
+app.use("/campgrounds/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 // 如果前面的route都没有被匹配
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404));
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    res.status(statusCode).render('error', { err });
-})
+  const { statusCode = 500 } = err;
+  res.status(statusCode).render("error", { err });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Serving on port ${ port }...`);
-})
+  console.log(`Serving on port ${port}...`);
+});
